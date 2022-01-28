616fc10b61e7c542e0d8995949373b70855bc5ab
Max Johnson <maxelliottjohnson@hotmail.co.uk>
2022-01-28 22:54:35 +0000

    Major refactor and addition of Blue Danube

---
+++ analysis.py
@@ -0,0 +1,78 @@
+from piece import Piece
+
+suku = Piece('suku', [3,3,3,3])
+suku.load_processed('Onset_time', 'Metric_location', 'Is_included_in_grid', 'Phase')
+
+manjanin = Piece('manjanin', [3,3,3,3])
+manjanin.load_processed('onsets_time', 'subdivision', 'is_valid_subdivision_assignment', 'relative_location_within_the_cycle_democratic')
+
+maraka = Piece('maraka', [3,3,3,3])
+maraka.load_processed('onsets_time', 'subdivision', 'is_valid_subdivision_assignment', 'relative_location_within_the_cycle_democratic')
+
+woloso = Piece('woloso', [3,3,3,3])
+woloso.load_processed('onsets_time', 'subdivision', 'is_valid_subdivision_assignment', 'relative_location_within_the_cycle_democratic')
+
+blue_danube_processed = Piece('blue-danube', [2,2,2], [1])
+blue_danube_processed.load_processed('Onset_time', 'Metric_location', 'Is_included_in_grid', 'Phase')
+
+blue_danube_unprocessed = Piece('blue-danube-unprocessed', [2,2,2], [1])
+blue_danube_unprocessed.load_from_onsets('Onset_time')
+
+# Suku
+def suku_plot():
+    suku.plot_histogram()
+
+def suku_tempo():
+    suku.tempo('Jembe-2', 95)
+
+def suku_mle():
+    suku.print_mle()
+
+
+# Manjanin
+def manjanin_plot():
+    manjanin.plot_histogram()
+
+def manjanin_tempo():
+    manjanin.tempo('Jembe2', 95.5)
+
+def manjanin_mle():
+    manjanin.print_mle()
+
+
+# Maraka
+def maraka_plot():
+    maraka.plot_histogram()
+
+def maraka_tempo():
+    maraka.tempo('Jembe2', 93)
+
+def maraka_mle():
+    maraka.print_mle()
+
+
+# Woloso
+def woloso_plot():
+    woloso.plot_histogram()
+
+def woloso_tempo():
+    woloso.tempo('Jembe2', 75)
+
+def woloso_mle():
+    woloso.print_mle()
+
+
+# Blue Danube
+def blue_danube_plot():
+    blue_danube_processed.plot_histogram(separately=True)
+
+def blue_danube_mle():
+    blue_danube_processed.print_mle()
+
+
+# Blue Danube (unprocessed)
+def blue_danube_unprocessed_plot():
+    blue_danube_unprocessed.plot_histogram(separately=True)
+
+def blue_danube_unprocessed_mle():
+    blue_danube_unprocessed.print_mle()
---
+++ piece.py
@@ -0,0 +1,267 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.optimize import curve_fit
+from scipy.stats import gaussian_kde, norm
+from sklearn.mixture import GaussianMixture
+from tabulate import tabulate
+
+
+# Class for performing micro-timing data analysis on a piece of music
+class Piece:
+
+    # name is the name of the subfolder within data/ that the piece's .csv files are found
+    # beat_divisions is a list of integers representing how many pulse units each beat in the metre can be divided into
+    # mixture_metric_locations is a list of indices representing which beats should be treated as a Gaussian mixture model instead of a Normal distribution
+    def __init__(self, name, beat_divisions, mixture_metric_locations=None):
+        self.name = name
+        self.beat_division = beat_divisions[0]
+        self.beats = len(beat_divisions)
+        self.pulse_units = sum(beat_divisions)
+        self.mixture_metric_locations = mixture_metric_locations
+
+    # Private method that loads all .csv files for a piece and concatenates them into one dataframe
+    # Can be filtered by any files with a given string in them
+    def _load_joined(self, filter=''):
+        filename = f'*{filter}*.csv' if filter else '*.csv'
+        files = Path().glob(f'data/{self.name}/{filename}')
+        self.df = pd.concat([pd.read_csv(file) for file in files])
+
+    # Private method that loads all .csv files for a piece and returns them as a list of dataframes
+    # Can be filtered by any files with a given string in them
+    def _load_separately(self, filter=''):
+        filename = f'*{filter}*.csv' if filter else '*.csv'
+        files = Path().glob(f'data/{self.name}/{filename}')
+        return [pd.read_csv(file) for file in files]
+
+    # Print the piece's dataframe (or optionally, any table recognised by tabulate) to the console
+    def print(self, table=None):
+        if table is not None:
+            print(tabulate(table, headers="keys"))
+        else:
+            print(tabulate(self.df, headers="keys"))
+
+    # Loads a piece which has already been processed
+    # Processed is defined as having columns for onsets, metric locations, validity, and phase of each note
+    # The names of the columns representing each of these must be passed in as strings
+    # Offset is calculated and it and Phase are converted to pulse units
+    def load_processed(self, onset=None, metric_loc=None, valid=None, phase=None):
+        if onset and metric_loc and valid and phase:
+            self._load_joined()
+            self.onset = onset
+            self.metric_loc = metric_loc
+            self.valid = valid
+            self.phase = phase
+            self.df['Offset'] = (self.df[self.phase] - self.df[self.metric_loc]) * self.beat_division
+            # Convert phase to be in pulse units
+            self.df[self.phase] = self.df[self.phase] * self.beat_division
+            # Filter invalid or nan values
+            self.df = self.df[self.df[self.valid] == 1]
+            self.df = self.df[self.df[self.phase].notna()]
+        else:
+            print("Please provide column names for onset times, metric locations, phase, and valid point")
+
+    # Loads a piece which is unprocessed, i.e. only has onset values
+    # Estimates metric locations and phase for data with onsets for only and all beats
+    # Assumes that only onsets on exact beats are included, the first onset is the first beat, and all beats are included with no discontinuities
+    def load_from_onsets(self, onset=None):
+        if onset:
+            dfs = self._load_separately()
+            for df in dfs:
+                # Remove any incomplete bars from the end (apart from its first note)
+                df.drop(df.tail((len(df) % self.beats) - 1).index, inplace=True)
+
+                # Perform calculations to estimate phase
+                df['Cycle_number'] = (df.index // self.beats) + 1
+                df['Metric_location'] = df.index % self.beats
+                cycle_start_index = df.index - df['Metric_location']
+                cycle_end_index = (cycle_start_index + self.beats) % len(df)
+                cycle_start_onset = df.iloc[cycle_start_index][onset].reset_index(drop=True)
+                cycle_end_onset = df.iloc[cycle_end_index][onset].reset_index(drop=True)
+                cycle_duration = cycle_end_onset - cycle_start_onset
+                isochronous_beat_duration = cycle_duration / self.beats
+                isochronous_onset = cycle_start_onset + (df['Metric_location'] * isochronous_beat_duration)
+                offset_seconds = df[onset] - isochronous_onset
+                df['Offset'] = (offset_seconds / isochronous_beat_duration) * self.beat_division
+                df['Phase'] = df['Offset'] + df['Metric_location']
+                df['Is_included_in_grid'] = 1
+
+                # Remove any incomplete bars from the end
+                df.drop(df.tail(len(df) % self.beats).index, inplace=True)
+
+                # Filter invalid or nan values
+                df = df[df['Phase'].notna()]    
+
+            self.df = pd.concat(dfs)
+
+            self.onset = onset
+            self.metric_loc = 'Metric_location'
+            self.valid = 'Is_included_in_grid'
+            self.phase = 'Phase'
+        else:
+            print("Please provide column name for onset times")
+
+    # Private method to use maximum likelihood estimation to fit and plot (on axis) a Normal distribution to a series
+    # Optionally also resamples values from the fitted distribution and plots these
+    def _plot_mle(self, series, axis, resample=False):
+        x_grid = np.linspace(min(series), max(series), 1000)
+        mean = series.mean()
+        stddev = series.std()
+        pdf = norm.pdf(x_grid, mean, stddev)
+        axis.plot(x_grid, pdf)
+        if resample:
+            axis.hist(np.random.normal(mean, stddev, len(series)), bins=20, density=True, stacked=True, alpha=0.5, label='MLE samples')
+            axis.legend()
+
+    # Private method to use maximum likelihood estimation to fit and plot (on axis) a Gaussian mixture model to a series
+    # Suitable for data which consists of multiple Normal distribution clusters within it
+    # Optionally also resamples values from the fitted distribution and plots these
+    def _plot_mixture_mle(self, series, axis, components=2, resample=False):
+        x_grid = np.linspace(min(series), max(series), len(series)).reshape(-1,1)
+        samples = np.array(series).reshape(-1, 1)
+        gm = GaussianMixture(components, covariance_type="spherical").fit(samples)
+        logprob = gm.score_samples(x_grid)
+        pdf = np.exp(logprob)
+        axis.plot(x_grid, pdf)
+        if resample:
+            axis.hist(gm.sample(len(series))[0], bins=20, density=True, stacked=True, alpha=0.5, label='MLE samples')
+            axis.legend()
+
+    # Private method to use kernel density estimation to fit and plot (on axis) a custom distribution to a series
+    # Suitable for data which does not fit a Normal (or any standard) distribution, or where the exact shape of the data should be preserved
+    # This approach was discarded in favour of maximum likelihood estimation
+    # Optionally also resamples values from the fitted distribution and plots these
+    def _plot_kde(self, series, axis, resample=False):
+        kde = gaussian_kde(series)
+        x_grid = np.linspace(min(series), max(series), 1000)
+        pdf = kde.evaluate(x_grid)
+        axis.plot(x_grid, pdf)
+        if resample:
+            axis.hist(kde.resample(len(series)).ravel(), bins=20, density=True, stacked=True, alpha=0.5, label='KDE samples')
+            axis.legend()
+
+    # Plots a histogram of the distributions of timings for each metric location, either on separate or a combined plot
+    # mle (maximum likelihood estimation) and kde (kernel density estimation) arguments being True will fit the corresponding distribution to the data and plot its PDF
+    # Optionally also resamples values from the fitted distribution and plots these
+    def plot_histogram(self, separately=False, mle=True, kde=False, resample=False):
+        df = self.df
+        metric_locations = df[self.metric_loc].unique()
+        metric_locations.sort()
+
+        if separately:
+            axs = df.hist('Offset', by=self.metric_loc, bins=20, density=True, stacked=True, alpha=0.5, label='data')
+            if mle or kde:
+                for i in range(len(metric_locations)):
+                    location = metric_locations[i]
+                    series = df[df[self.metric_loc] == location]['Offset']
+                    if mle:
+                        if self.mixture_metric_locations and location in self.mixture_metric_locations:
+                            self._plot_mixture_mle(series, axs.flat[i], resample=resample)
+                        else:
+                            self._plot_mle(series, axs.flat[i], resample=resample)
+                    if kde:
+                        self._plot_kde(series, axs.flat[i], resample=resample)
+            plt.show()
+        else:
+            for location in metric_locations:
+                series = df[df[self.metric_loc] == location][self.phase]
+                plt.hist(series, bins=20, density=True, stacked=True, alpha=0.5, label='data')
+                if mle:
+                    if self.mixture_metric_locations and location in self.mixture_metric_locations:
+                        self._plot_mixture_mle(series, plt)
+                    else:
+                        self._plot_mle(series, plt)
+                if kde:
+                    self._plot_mle(series, plt)
+            plt.xticks(np.arange(0, self.pulse_units, 1.0))
+            plt.show()
+
+    # Prints maximum likelihood estimates of the mean and standard deviation of a fitted Normal distribution for each metric location
+    # If a Gaussian mixture model was used, the component with smallest mean is chosen
+    # Data is printed in a CSV-like format
+    def print_mle(self):
+        df = self.df
+        metric_locations = df[self.metric_loc].unique()
+        metric_locations.sort()
+
+        print('Beat index,Pulse unit index,Mean,Standard deviation')
+        for location in metric_locations:
+            series = df[df[self.metric_loc] == location]['Offset']
+            if self.mixture_metric_locations and location in self.mixture_metric_locations:
+                samples = np.array(series).reshape(-1, 1)
+                gm = GaussianMixture(2, covariance_type="spherical").fit(samples)
+                component_index = np.argmin(gm.means_)
+                mean = gm.means_[component_index][0]
+                stddev = gm.covariances_[component_index]
+            else:
+                mean = series.mean()
+                stddev = series.std()
+            beat = int(np.floor(location))
+            pulse_unit = int(np.rint((10/self.beat_division) * (location - beat)))
+            print(beat,',',pulse_unit,',',mean,',',stddev, sep='')
+
+    # Private method for evaluating a general natural logarithmic function
+    def _logf(self, x, a, b, c):
+        return a * np.log(x + b) + c
+
+    # Private method for evaluating a general quadratic function
+    def _quadraticf(self, x, a, b, c):
+        return (a * (x**2)) + (b * x) + c
+
+    # Private method for evaluating a general logarithmic function below tempo_cutoff, and a general quadratic function above
+    def _combinedf(self, x, a, b, c, d, e, f):
+        return np.where(x < self.tempo_cutoff, self._logf(x,a,b,c), self._quadraticf(x,d,e,f))
+
+    # Tempo analysis for Jembe music
+    # Analyses the changing tempo according to beat_instrument, which is any instrument which plays on each beat (often Jembe 2)
+    # Assumes tempo can be modelled as roughly logarithmic until tempo_cutoff %, after which it is quadratic
+    # i.e. y = a log(x+b) + c for x < tempo_cutoff, and y = dx^2 + ex + f for x >= tempo_cutoff
+    # Plots tempo curve for each take and the average tempo, fits curves to the average tempo and plots them, and prints parameters
+    # Tempo is averaged with a sliding window of size 10
+    # Also calculates and prints average duration of the piece across all takes
+    def tempo(self, beat_instrument, tempo_cutoff):
+        dfs = self._load_separately(beat_instrument)
+        self.tempo_cutoff = tempo_cutoff
+        dfs_new = []
+        for df in dfs:
+            df.drop(df[df[self.valid] != 1].index, inplace=True)
+            # Filter to just beats
+            df.drop(df[~df[self.metric_loc].isin(np.arange(self.beats))].index, inplace=True)
+            # Redefine Onset_time column to allow shift
+            df['Onset_time'] = df[self.onset]
+            df['Tempo'] = 60 / (df.Onset_time - df.Onset_time.shift(1))
+            first_onset = df[self.onset].iloc[0]
+            last_onset = df[self.onset].iloc[-1]
+            duration = last_onset - first_onset
+            df['Duration'] = duration
+            # Percentage progress throughout the piece
+            progress = ((df[self.onset] - first_onset) / duration) * 100
+            df['Progress'] = progress.round(1)
+            df = df.groupby(['Progress'], as_index=False).mean()
+            df['Average Tempo'] = df['Tempo'].rolling(window=10).mean()
+            plt.plot(df['Progress'], df['Average Tempo'], linewidth=0.5, alpha=0.5, color='gray')
+            dfs_new.append(df)
+        df = pd.concat(dfs_new)
+
+        m = df.groupby(['Progress'], as_index=False).mean()
+        m['Average Tempo'] = m['Tempo'].rolling(window=10).mean()
+        plt.plot(m['Progress'], m['Average Tempo'])
+
+        m = m.dropna()
+        popt,_ = curve_fit(self._combinedf, m['Progress'], m['Average Tempo'])
+        a,b,c,d,e,f = popt
+        print('Params:')
+        print('a =', a)
+        print('b =', b)
+        print('c =', c)
+        print('d =', d)
+        print('e =', e)
+        print('f =', f)
+        plt.plot(m['Progress'], self._combinedf(m['Progress'],a,b,c,d,e,f))
+
+        dur = df['Duration'].unique()
+        print('Mean duration:', dur.mean())
+
+        plt.show()
